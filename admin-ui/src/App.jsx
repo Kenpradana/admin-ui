c3290a6612a4382f44008ec60f31684eb3c75824
Kenpradana <kenpradana8880@gmail.com>
2026-05-19 01:30:19 +0700

    update react dom

--- admin-ui/src/App.jsx
+++ admin-ui/src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.css";
-import SignInPage from "./pages/signIn";
-import SignUpPage from "./pages/signUp";
+// PERHATIKAN BAGIAN INI: Sudah saya ubah jadi huruf kecil semua (signin, signup, error)
+import SignInPage from "./pages/signin";
+import SignUpPage from "./pages/signup";
 import ErrorPage from "./pages/error";
+
 import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
 
 function App() {
@@ -9,12 +11,12 @@ function App() {
     {
       path: "/",
       element: (
-        	<div className="flex justify-center items-center min-h-screen">
-          <Link to="/login" className="p-2 m-5 bg-[#299d91]  text-white">
+        <div className="flex justify-center items-center min-h-screen">
+          <Link to="/login" className="p-2 m-5 bg-[#299d91] text-white rounded">
             Login
           </Link>
           |
-          <Link to="/register" className="p-2 m-5 bg-[#299d91]  text-white">
+          <Link to="/register" className="p-2 m-5 bg-[#299d91] text-white rounded">
             Register
           </Link>
         </div>
