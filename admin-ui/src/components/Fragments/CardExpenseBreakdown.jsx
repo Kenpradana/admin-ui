75f1d006c4686179761ff5e744ab8c5e52fcb967
Kenpradana <kenpradana8880@gmail.com>
2026-06-02 02:14:48 +0700

    update perbaikan card

--- admin-ui/src/components/Fragments/CardExpenseBreakdown.jsx
+++ admin-ui/src/components/Fragments/CardExpenseBreakdown.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "../Elements/Card";
-import Icon from "../Elements/icon";
+import Icon from "../Elements/Icon";
 
 function CardExpenseBreakdown(props) {
   const { data } = props;
